6d01d46ad841f7f65078384d90e3835e45f2d929
Prateek Dixit <65072849+Prateekdixit200@users.noreply.github.com>
2026-09-13 13:19:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,5 +112,5 @@ def get_releases():
     })
 
 if __name__ == '__main__':
-    # Using port 5000 as default for Flask development
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
